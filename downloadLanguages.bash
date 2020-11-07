9c2b1e9f21dd62e38d5a752ac050cfe16a73f96a
ysl3000 <yannicklamprecht@live.de>
2020-11-07 18:13:07 +0100

    added empty readme and license, allow direct maven execution without build

--- downloadLanguages.bash
+++ downloadLanguages.bash
@@ -5,5 +5,5 @@ if [ "$#" -ne 2 ]; then
     exit 0
 fi
 
-# mvn exec:java -Dmcver="$1" -DoutDir="$2" -Dexec.mainClass="LocalizationDownloader"
-java -Dmcver="$1" -DoutDir="$2" -cp target/mc-localization-1.0-SNAPSHOT.jar LocalizationDownloader
+mvn exec:java -Dmcver="$1" -DoutDir="$2" -Dexec.mainClass="LocalizationDownloader"
+#java -Dmcver="$1" -DoutDir="$2" -cp target/mc-localization-1.0-SNAPSHOT.jar LocalizationDownloader
